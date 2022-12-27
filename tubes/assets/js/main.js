334fb4d178dcb7ba715a56c1df4dfb5af8abdbb8
Warlord7777 <117634551+Warlord7777@users.noreply.github.com>
2022-12-27 17:26:23 +0700

    Delete tubes directory

--- tubes/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Selector input yang akan menampilkan autocomplete.
-    $("#automplete-2").autocomplete({
-        source: "../../includes/autocomplete.php",
-        minLength: 2
-    });
-})
--- tubes/hapusproduk.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$id = $_GET["id"];
-unset($_SESSION["keranjang"][$id]);
-
-echo "<script>alert('Produk anda dihapus dari keranjang');</script>";
-echo "<script>location='keranjang.php';</script>"
-
-?>
