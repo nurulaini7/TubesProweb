cafd368330b05a7e25403d84980dfd3e4948c231
nurulaini7 <119827418+nurulaini7@users.noreply.github.com>
2022-12-28 11:00:15 +0700

    Update index.php

--- tubes/index.php
+++ tubes/index.php
@@ -14,11 +14,11 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'>
     <link rel="stylesheet" href="assets/css/style.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <title>Eracom</title>
   
-
     <!-- icons -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
@@ -52,10 +52,10 @@
               </li>
             </li>
             <li class="nav-item mt-2">
-              <a class="nav-link" href="#">Login</a>
+              <a class="nav-link" href="#">Register</a>
             </li>
             <li class="nav-item mt-2">
-              <a class="nav-link" href="#">Register</a>
+              <a class="nav-link" href="#">Login</a>
             </li>
             <div class="icons-home nav-icons">
               <a href="keranjang.php"><i class='bx bxs-cart'></i></i></a>
@@ -194,13 +194,35 @@
     <!-- akhir Produk -->
     
     <!-- Awal Footer -->
-    <footer>
+     <footer>
       <div class="container">
         <div class="footer-content row mb-4">
           <div class="footer-brand col-12 col-sm-12 col-md-3 col-lg-3">
             <div>
-              <h1 class="text-main">ERACOM</h1>
+            <img src="assets/img/logo.png" alt="" width="300px" height="100px" />
+            <br><br>
+            <h6 class="text-white">Eracom merupakan website toko online yang menjual perangkat elektronik berupa laptop, kami menyediakan kebutuhan anda pada era industri 4.0 </h6>
+            <br><br> 
+            <div class="color-white">
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                <i class="fab fa-github"></i>
+              </a>
             </div>
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          </div>
+          </div>
           </div>
   
           <div class="footer-items-box col-12 col-sm-12 col-md-9 col-lg-9">
@@ -208,56 +230,72 @@
               <div class="footer-item col-12 col-sm-12 col-md-4">
                 <div>
                   <div class="footer-item-content">
-                    <h3 class="text-main">Anggota Kelompok</h3>
-                    <p>Hanzel Oclihar Tjiam</p>
-                    <p>Umar Hilmi</a></p>
-                    <p>Nurul Aini</a></p>
-                    <p>Annisa Cahyani</a></p>
-                    <p>Jungjungan Hans Aryanta Silitonga</a></p>
-                    <p>Firman Ramadhani Yusma</a></p>
+                    <h3 class="text-main">Fitur</h3>
+                    <p>Home</p>
+                    <p>Kategori Produk</a></p>
+                    <p>Keranjang</a></p>
+                    <p>Register</a></p>
+                    <p>Login</a></p>
+                    <p>Pencarian</a></p>
                   </div>
                 </div>
               </div>
     
-              <div class="footer-item col-12 col-sm-12 col-md-4">
-                <div>
-                  <div class="footer-item-content">
-                    <h3 class="text-main">Business</h3>
-                    <p><a href="#">eracom22@gmail.com</a></p>
-                    <p><a href="#">021-1234-5678</a></p>
-                    <p><a href="#">Medan, Sumatera Utara</a></p>
+                      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase mb-4 text-white">Kontak Kami</h5>
+                        <ul class="fa-ul text-white" style="margin-left: 1.65em;">
+                          <li class="mb-3">
+                            <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Medan, 00-967, Sumatera Utara</span>
+                          </li>
+                          <li class="mb-3">
+                            <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">Eracom22@gmail.com</span>
+                          </li>
+                          <li class="mb-3">
+                            <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+62 8123 456 789</span>
+                          </li>
+                        </ul>
+                      </div>
+
+                            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase mb-4 text-white">Jam Operasional</h5>
+
+                            <table class="table text-center text-white">
+                              <tbody class="fw-normal">
+                                <tr>
+                                  <td>Mon - Thu:</td>
+                                  <td>8am - 9pm</td>
+                                </tr>
+                                <tr>
+                                  <td>Fri - Sat:</td>
+                                  <td>8am - 9pm</td>
+                                </tr>
+                                <tr>
+                                  <td>Sunday:</td>
+                                  <td>9am - 10pm</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                </div>
               </div>
-    
-              <div class="footer-item col-12 col-sm-12 col-md-4">
-                <div>
-                  <div class="footer-item-content">
-                    <h3 class="text-main">About</h3>
-                    
-                      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem fuga laboriosam deserunt culpa, eius magnam adipisci veritatis consectetur cupiditate iusto amet incidunt sapiente nobis sed, nihil labore! Harum, deleniti repellendus!</p>
-                    
+                      
+              <div class="copyright-section border-top">
+                <div class="row">
+                  <div class="col-12">
+                    <div class="copyright-content text-center mt-4">
+                      <p class="text-second">Eracom Online Store Copyright &copy; 2022 All Rights Reserved</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-  
-        <div class="copyright-section border-top">
-          <div class="row">
-            <div class="col-12">
-              <div class="copyright-content text-center mt-4">
-                <p class="text-second">Eracom Online Store Copyright &copy; 2022 All Rights Reserved</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    
-
+          </footer>
+<!-- Akhir footer -->
     
     <script src="assets/js/main.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
