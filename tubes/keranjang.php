552d78e4c09abf11361e6b9e9c274c07319c2b61
nurulaini7 <119827418+nurulaini7@users.noreply.github.com>
2022-12-28 11:08:07 +0700

    Update keranjang.php

--- tubes/keranjang.php
+++ tubes/keranjang.php
@@ -9,6 +9,7 @@ $koneksi = new mysqli ("localhost", "root", "", "eracom");
 <head>
     <title>keranjang</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'>
     <link rel="stylesheet" href="assets/css/style.css" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
